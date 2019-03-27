3e37bc52a94d552e5530650486b1b5f1a2333c7e
Personal <James.A.Quigley@asu.edu>
2019-03-27 13:56:58 -0400

    Random color

---
+++ color.js
@@ -0,0 +1,12 @@
+function randomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+module.exports = (req, res) => {
+  res.end({
+    r: randomInt(256),
+    g: randomInt(256),
+    b: randomInt(256),
+    a: randomInt(256)
+  })
+}
